5ceb621ebc6fa875c3d317e97bc25917c1c349b3
khadiza1216 <155760201+khadiza1216@users.noreply.github.com>
2024-01-17 21:16:05 +0600

    Add files via upload

---
+++ UVA 11172.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+long long int t,a,b;
+scanf("%lld",&t);
+while(t--)
+{scanf("%lld %lld",&a,&b);
+
+    if (a>b)
+{
+    printf(">\n");
+}
+else if (a<b)
+{
+        printf("<\n");
+}
+else
+{
+        printf("=\n");
+}
+}
+return 0;
+}
